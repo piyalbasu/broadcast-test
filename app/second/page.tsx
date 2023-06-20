1d12866ee3c0d5b4295eca8a9314b84ee79b61b5
Piyal Basu <pbasu235@gmail.com>
2023-06-20 18:24:09 -0400

    adding broadcast poc

--- app/second/page.tsx
+++ app/second/page.tsx
@@ -1,20 +1,21 @@
 "use client";
 
-import { useState } from "react";
+import { useState, useEffect } from "react";
+
+import Image from "next/image";
 
 export default function Home() {
   const [status, setStatus] = useState(0);
-
-  const handleClick = () => {
-    setStatus(status + 1);
-
+  useEffect(() => {
     const bc = new BroadcastChannel("test_channel");
-    bc.postMessage(status);
-  };
+    bc.onmessage = (event) => {
+      setStatus(event.data);
+    };
+  }, []);
 
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <button onClick={handleClick}>click here</button>
+      status: {status}
     </main>
   );
 }
